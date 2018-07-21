fab4447d2cd548c351edb098e02dfc32e19af288
Anastasiia <nastena31011993@gmail.com>
2018-07-21 20:39:07 +0300

    Added fighter and improved fighter classes

--- src/public/fighter.js
+++ src/public/fighter.js
@@ -1,1 +1,29 @@
 // Create class Fighter
+class Fighter {
+    constructor(name, power, health){
+        this.name = name;
+        this.health = health;
+        this.power = power;
+    }
+
+    setDamage(damage) {
+        this.health = this.health - damage;
+        console.log('${this.name} health: ${this.health}');
+    }
+
+    hit(enemy, point) {
+        damage = point * this.power;
+        enemy.setDamage(damage);
+    }
+
+    knockout() {
+        console.log('time is over');
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("game over");
+            }, 500);
+        });
+
+        return promise;
+    }
+}
--- src/public/improvedFighter.js
+++ src/public/improvedFighter.js
@@ -1,1 +1,11 @@
 // Create class ImprovedFighter
+class ImprovedFighter extends Fighter {
+    constructor(name, power, health) {
+        super(name, power, health);
+    }
+
+    doubleHit(enemy, point) {
+        point = point * 2;
+        super.hit(enemy, point);
+    }
+}
